964a8722e9c50e0b18151bfe11f197791d4b20be
jason <jason@Mastercomputer>
2026-03-21 15:50:12 -0400

    added availability zones

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
   
   tags = {
     Name = "Public-Subnet-1"
@@ -30,6 +31,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
   
   tags = {
     Name = "Public-Subnet-2"
@@ -39,6 +41,7 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
   
   tags = {
     Name = "Private-Subnet-1"
@@ -48,7 +51,8 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.4.0/24"
-  
+  availability_zone = "us-east-1b"
+    
   tags = {
     Name = "Private-Subnet-2"
   }
