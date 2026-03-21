388775578cd631c3d875e536ee3092d9002cb02c
jason <jason@Mastercomputer>
2026-03-21 14:14:34 -0400

    main.tf created.

---
+++ main.tf
@@ -0,0 +1,234 @@
+#AWS infrastructure
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "DevOps-Project-VPC"
+  }
+}
+
+	#Subnet ID's / tags
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.3.0/24"
+  
+  tags = {
+    Name = "Private-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.4.0/24"
+  
+  tags = {
+    Name = "Private-Subnet-2"
+  }
+}
+
+	# Routing / Gateways
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "DevOps-Project-IGW"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+	#Public
+
+resource "aws_route_table_association" "public_rta_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rta_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "NAT-Gateway"
+  }
+}
+
+	#Private
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "Private-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "private_rta_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rta_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# bastion infrastructure
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-0c7217cdde317cfec" # standard Ubuntu image for us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  
+  tags = {
+    Name = "Bastion-Host"
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name   = "bastion-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Jenkins Infrastructure
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.medium" # hopefully no slow jenkins?
+  subnet_id     = 
+  
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name   = "jenkins-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+  
+  ingress {
+    from_port   = 8080   # We'll likely need port 8080 later for the Jenkins
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Allow internal VPC traffic to see the UI
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#SonarQube Infrastructure
+
+resource "aws_instance" "sonarqube" {
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.medium" 
+  subnet_id              = aws_subnet.private_subnet_2.id
+  vpc_security_group_ids = [aws_security_group.sonarqube_sg.id]
+
+  tags = {
+    Name = "SonarQube-Server"
+  }
+}
+
+resource "aws_security_group" "sonarqube_sg" {
+  name   = "sonarqube-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  # SSH access only from Bastion
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  # SonarQube UI/API access from Jenkins
+  ingress {
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
